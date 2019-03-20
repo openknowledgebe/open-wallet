80e6d701a04aefab7f24d5e1c664e78071283358
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-20 14:12:41 +0100

    Create mongoose Address schema

---
+++ backend/src/models/address.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema(
+  {
+    street: String,
+    city: String,
+    country: String,
+    zipCode: Number
+  },
+  { _id: false }
+);
+
+module.exports = addressSchema;

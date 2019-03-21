19e33ca06ecef22ccf3920a915b678ed60fc3510
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-21 11:05:15 +0100

    Further implement register mutation

--- backend/src/graphql/resolvers/mutations.js
+++ backend/src/graphql/resolvers/mutations.js
@@ -1,7 +1,24 @@
+const bcrypt = require("bcrypt");
 const User = require("../../models/user");
 
 module.exports = {
-  register: (_, { user }) => {
-    return new User(user).save();
+  register: async (_, { user }) => {
+    // TODO Valid email format
+    // TODO Password is strong enough
+    // lowercase the email
+    const email = user.email.toLowerCase();
+    // Make sure any user has the email
+    const dupUser = await User.findOne({ email });
+    if (dupUser) {
+      throw new Error("An account with that email address already exists.");
+    }
+    // Hash Password
+    const password = await bcrypt.hash(user.password, 10);
+    // Register && Return the user
+    return User({
+      ...user,
+      email,
+      password
+    }).save();
   }
 };

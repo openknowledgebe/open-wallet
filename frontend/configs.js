fee5c549a7f5d794eecc475113858cd76ac1104e
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-25 16:16:52 +0100

    Modify lint-staged configuration

--- frontend/configs.js
+++ frontend/configs.js
@@ -1,1 +1,2 @@
-export const endpoint = "http://localhost:4000/graphql";
+// eslint-disable-next-line import/prefer-default-export
+export const endpoint = 'http://localhost:4000/graphql';
--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -1,17 +1,15 @@
-import withApollo from "next-with-apollo";
-import ApolloClient, { InMemoryCache } from "apollo-boost";
-import { endpoint } from "../configs";
+import withApollo from 'next-with-apollo';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { endpoint } from '../configs';
 
 export default withApollo(
+  // eslint-disable-next-line no-unused-vars
   ({ ctx, headers, initialState }) =>
     new ApolloClient({
-      uri:
-        process.env.NODE_ENV === "development"
-          ? endpoint
-          : process.env.ENDPOINT,
+      uri: process.env.NODE_ENV === 'development' ? endpoint : process.env.ENDPOINT,
       cache: new InMemoryCache().restore(initialState || {}),
       fetchOptions: {
-        credentials: "include"
+        credentials: 'include'
       }
     })
 );

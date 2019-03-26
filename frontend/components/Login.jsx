e818991dbdf3d8315b465e62d155f276cb886b12
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-26 15:36:43 +0100

    Create Login component

---
+++ frontend/components/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FormContainer from './FormContainer';
+
+const Login = () => {
+  return <FormContainer>This is the login form</FormContainer>;
+};
+
+export default Login;
--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,5 +1,12 @@
+import Login from '../components/Login';
+
 function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      Welcome to Next.js!
+      <Login />
+    </div>
+  );
 }
 
 export default Home;

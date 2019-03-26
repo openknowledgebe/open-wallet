d6491f95eff6b70ab9ad18c0b37c9c8802ad6a94
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-26 16:32:40 +0100

    Implement Login component

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { FormGroup, InputGroup, Button, Intent } from '@blueprintjs/core';
 import FormContainer from './FormContainer';
 
 const Login = () => {
-  return <FormContainer>This is the login form</FormContainer>;
+  return (
+    <FormContainer>
+      <form method="post">
+        <FormGroup label="Email" labelFor="email">
+          <InputGroup large type="email" id="email" placeholder="Your email address" />
+        </FormGroup>
+        <FormGroup label="Password" labelFor="password">
+          <InputGroup large type="password" id="password" placeholder="Your password" />
+        </FormGroup>
+        <Button large type="submit" intent={Intent.PRIMARY}>
+          Sign in
+        </Button>
+      </form>
+    </FormContainer>
+  );
 };
 
 export default Login;

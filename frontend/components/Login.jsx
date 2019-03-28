a035c80dcc34cc862ad2272253af01422546b6d2
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-03-28 16:57:09 +0100

    Implement login logic

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,22 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormGroup, InputGroup, Button, Intent } from '@blueprintjs/core';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+const LOG_ME_IN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      firstName
+    }
+  }
+`;
+
+const useFormInput = initVal => {
+  const [value, setValue] = useState(initVal);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+};
 
 const Login = () => {
+  const email = useFormInput('');
+  const password = useFormInput('');
   return (
-    <div>
-      <h1>Sign in</h1>
-      <form method="post">
-        <FormGroup label="Email" labelFor="email">
-          <InputGroup large type="email" id="email" placeholder="Your email address" />
-        </FormGroup>
-        <FormGroup label="Password" labelFor="password">
-          <InputGroup large type="password" id="password" placeholder="Your password" />
-        </FormGroup>
-        <Button large type="submit" intent={Intent.PRIMARY}>
-          Sign in
-        </Button>
-      </form>
-    </div>
+    <Mutation mutation={LOG_ME_IN} variables={{ email: email.value, password: password.value }}>
+      {/* TODO handle error */}
+      {/* TODO redirect to user home page */}
+      {(login, { loading }) => {
+        return (
+          <div>
+            <h1>Sign in</h1>
+            <form
+              method="post"
+              onSubmit={e => {
+                e.preventDefault();
+                login();
+              }}
+            >
+              <FormGroup label="Email" labelFor="email" disabled={loading}>
+                <InputGroup
+                  disabled={loading}
+                  large
+                  type="email"
+                  id="email"
+                  placeholder="Your email address"
+                  {...email}
+                />
+              </FormGroup>
+              <FormGroup label="Password" labelFor="password" disabled={loading}>
+                <InputGroup
+                  disabled={loading}
+                  large
+                  type="password"
+                  id="password"
+                  placeholder="Your password"
+                  {...password}
+                />
+              </FormGroup>
+              <Button large type="submit" loading={loading} intent={Intent.PRIMARY}>
+                Sign in
+              </Button>
+            </form>
+          </div>
+        );
+      }}
+    </Mutation>
   );
 };
 

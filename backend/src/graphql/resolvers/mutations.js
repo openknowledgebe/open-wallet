ea76b4b44b0ca674dfe75437227ff8509ef8991a
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-02 13:34:04 +0200

    Move login, logout queries to mutations

--- backend/src/graphql/resolvers/mutations.js
+++ backend/src/graphql/resolvers/mutations.js
@@ -1,3 +1,9 @@
 module.exports = {
-  register: async (_, { user }, { models: { User } }) => User(user).save()
+  register: async (_, { user }, { models: { User } }) => User(user).save(),
+  logout: (_, args, { res, auth }) => {
+    return auth.attemptLogout(res);
+  },
+  login: async (_, { email, password }, { res, models: { User }, auth }) => {
+    return auth.attemptLogin(email, password, res, User);
+  }
 };
--- backend/src/graphql/resolvers/queries.js
+++ backend/src/graphql/resolvers/queries.js
@@ -1,10 +1,4 @@
 module.exports = {
   users: async (root, args, { models: { User } }) => User.find(),
-  login: async (_, { email, password }, { res, models: { User }, auth }) => {
-    return auth.attemptLogin(email, password, res, User);
-  },
-  me: (_, args, { user }) => user,
-  logout: (_, args, { res, auth }) => {
-    return auth.attemptLogout(res);
-  }
+  me: (_, args, { user }) => user
 };

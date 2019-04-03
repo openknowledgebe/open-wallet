d3c67114e03a848c14aeaf2ab9bb3b367598c33d
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-03 12:05:08 +0200

    Use FormContainer & Page container

---
+++ frontend/pages/profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from '../components/Profile';
+import Page from '../components/Page';
+
+const ProfilePage = () => {
+  return (
+    <Page>
+      <Profile />
+    </Page>
+  );
+};
+
+export default ProfilePage;

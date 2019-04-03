01df5acf1aa21058d77b419fa2557156963b5923
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-03 09:35:23 +0200

    Add utils.js to provide testing utilities

---
+++ backend/src/tests/utils.js
@@ -0,0 +1,85 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { HttpLink } = require('apollo-link-http');
+const fetch = require('node-fetch');
+const { execute, toPromise } = require('apollo-link');
+
+module.exports.toPromise = toPromise;
+
+const {
+  context: defaultContext,
+  typeDefs,
+  resolvers,
+  ApolloServer,
+  app,
+  models,
+  schemaDirectives
+} = require('../');
+
+/**
+ * Integration testing utils
+ */
+const constructTestServer = ({ context = defaultContext } = {}) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    schemaDirectives,
+    context
+  });
+
+  return { server };
+};
+
+module.exports.constructTestServer = constructTestServer;
+
+/**
+ * e2e Testing Utils
+ */
+
+const startTestServer = async server => {
+  server.applyMiddleware({ app });
+  const httpServer = await app.listen(0);
+
+  const link = new HttpLink({
+    uri: `http://localhost:${httpServer.address().port}${server.graphqlPath}`,
+    credentials: 'include',
+    fetch
+  });
+
+  const executeOperation = ({ query, variables = {} }) => execute(link, { query, variables });
+
+  return {
+    link,
+    stop: async () => {
+      httpServer.close();
+    },
+    graphql: executeOperation
+  };
+};
+
+module.exports.startTestServer = startTestServer;
+
+// populate database
+
+const populate = async () => {
+  const { User } = models;
+  const count = await User.countDocuments();
+  if (!count) {
+    await User({
+      name: 'John Doe',
+      email: 'john.doe@john.com',
+      password: 'johnnyyyyy'
+    }).save();
+    await User({
+      name: 'Sylvie Delcourt',
+      email: 'Sylvie@hotmail.com',
+      password: 'sylvie2324'
+    }).save();
+    await User({
+      name: 'Mitch Mitch',
+      email: 'mitchell@gmail.com',
+      password: 'mitchelll'
+    }).save();
+  }
+};
+
+module.exports.populate = populate;

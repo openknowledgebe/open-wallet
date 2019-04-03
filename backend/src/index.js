a17ea8934db56237750200483e94a65ea63d9d7f
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-03 09:31:24 +0200

    Extract context to be able to export it

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,12 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cookieParser());
 
+const context = async ({ req, res }) => {
+  const user = await auth.loggedUser(req.cookies, models);
+  // adopting injection pattern to ease mocking
+  return { req, res, user, auth, models };
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
@@ -25,11 +31,7 @@ const server = new ApolloServer({
     Mutation
   },
   schemaDirectives,
-  context: async ({ req, res }) => {
-    const user = await auth.loggedUser(req.cookies, models);
-    // adopting injection pattern to ease mocking
-    return { req, res, user, auth, models };
-  },
+  context,
   mocks: false
 });
 
@@ -40,7 +42,7 @@ if (process.env.NODE_ENV !== 'test') {
   db.connect();
 
   app.listen({ port: PORT }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
   );
 }
 
@@ -52,6 +54,8 @@ module.exports = {
     Query,
     Mutation
   },
+  context,
+  ApolloServer,
   schemaDirectives,
   db,
   auth,

974147d9a774c3f2fa395f61b4a1f81606858f78
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-09 16:10:24 +0200

    Add middleware to unable access for unauthenticated users

--- frontend/pages/profile.jsx
+++ frontend/pages/profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Profile from '../components/Profile';
 import Page from '../components/Page';
+import redirect from '../lib/redirect';
+import checkLoggedIn from '../lib/checkLoggedIn';
 
 const ProfilePage = () => {
   return (
@@ -8,6 +10,17 @@ const ProfilePage = () => {
       <Profile />
     </Page>
   );
+};
+
+ProfilePage.getInitialProps = async context => {
+  const { loggedInUser } = await checkLoggedIn(context.apolloClient);
+  if (!loggedInUser.me) {
+    // Already logged in? No need to continue.
+    // Throw them back to the main page
+    redirect(context, '/login');
+  }
+
+  return {};
 };
 
 export default ProfilePage;

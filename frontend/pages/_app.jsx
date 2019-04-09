55223591b8fb39539f97705067e870cd7695df32
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-09 16:05:13 +0200

    Remove Page component

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -3,7 +3,6 @@ import App, { Container } from 'next/app';
 import { ApolloProvider } from 'react-apollo';
 import { FocusStyleManager } from '@blueprintjs/core';
 import withApollo from '../lib/withApollo';
-import Page from '../components/Page';
 import 'normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
@@ -27,9 +26,7 @@ class MyApp extends App {
     return (
       <Container>
         <ApolloProvider client={apollo}>
-          <Page>
-            <Component {...pageProps} />
-          </Page>
+          <Component {...pageProps} />
         </ApolloProvider>
       </Container>
     );

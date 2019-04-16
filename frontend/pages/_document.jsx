4f9ef16431a90d70f93a089fa2177271cb13bf64
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-16 10:00:22 +0200

    Remove title to fix warning

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -33,7 +33,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <title>Open Wallet</title>
           <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
         </Head>
         <body>

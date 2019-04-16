01b9b81e68340e54d80a466f1b23b55e08ffb2d9
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-16 10:04:57 +0200

    Remove FormContainer as the component wrapper

--- frontend/components/Profile/Profile.jsx
+++ frontend/components/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import FormContainer from '../FormContainer';
 import General from './General';
 import Address from './Address';
 import BankDetails from './BankDetails';
@@ -10,12 +9,12 @@ const Profile = ({ user }) => {
   const { address, bankDetails } = user;
 
   return (
-    <FormContainer>
+    <>
       <General name={user.name} email={user.email} />
       <Address address={address} />
       <BankDetails bankDetails={bankDetails} />
       <Password />
-    </FormContainer>
+    </>
   );
 };
 

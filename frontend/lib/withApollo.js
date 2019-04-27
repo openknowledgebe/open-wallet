a903fbde1ff2e01570cadc0007283b57a9feb6d1
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-27 20:59:54 +0200

    Rewrite withApollo HOC to enable file upload

--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -1,23 +1,32 @@
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import withApollo from 'next-with-apollo';
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import { endpoint } from '../configs';
 
-export default withApollo(
-  // eslint-disable-next-line no-unused-vars
-  ({ ctx, headers, initialState }) =>
-    new ApolloClient({
-      uri: process.env.NODE_ENV === 'development' ? endpoint : process.env.ENDPOINT,
-      request: operation => {
-        operation.setContext({
-          fetchOptions: {
-            credentials: 'include'
-          },
-          headers
-        });
-      },
-      cache: new InMemoryCache().restore(initialState || {})
-    }),
-  {
-    getDataFromTree: 'ssr'
-  }
-);
+export default withApollo(({ initialState, headers = {} }) => {
+  const httpLink = createUploadLink({
+    uri: process.env.NODE_ENV === 'development' ? endpoint : process.env.ENDPOINT,
+    fetchOptions: { credentials: 'include' }
+  });
+
+  const contextLink = setContext(async () => ({
+    headers
+  }));
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(err => console.log(`[GraphQL error]: Message: ${err.message}`));
+    }
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
+
+  const link = ApolloLink.from([errorLink, contextLink, httpLink]);
+
+  const cache = new InMemoryCache().restore(initialState || {});
+
+  return new ApolloClient({ link, cache });
+});

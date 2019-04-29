eb257c63ccf15989f988af610c6a68da2f5bb324
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-29 14:12:34 +0200

    Use grid to render profile page forms

--- frontend/components/Profile/Profile.jsx
+++ frontend/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import General from './General';
 import Address from './Address';
 import BankDetails from './BankDetails';
@@ -9,12 +10,20 @@ const Profile = ({ user }) => {
   const { address, bankDetails } = user;
 
   return (
-    <>
-      <General name={user.name} email={user.email} />
-      <Address address={address} />
-      <BankDetails bankDetails={bankDetails} />
-      <Password />
-    </>
+    <Grid padded>
+      <Grid.Column mobile={16} tablet={8} computer={8} largeScreen={8}>
+        <General name={user.name} email={user.email} />
+      </Grid.Column>
+      <Grid.Column mobile={16} tablet={8} computer={8} largeScreen={8}>
+        <Address address={address} />
+      </Grid.Column>
+      <Grid.Column mobile={16} tablet={8} computer={8} largeScreen={8}>
+        <BankDetails bankDetails={bankDetails} />
+      </Grid.Column>
+      <Grid.Column mobile={16} tablet={8} computer={8} largeScreen={8}>
+        <Password />
+      </Grid.Column>
+    </Grid>
   );
 };
 

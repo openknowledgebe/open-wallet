58bb8dbd01927dfdb34ae26c764f708346153f49
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-04-29 15:29:57 +0200

    Add ssr flag to only request once

--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -7,26 +7,29 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import withApollo from 'next-with-apollo';
 import { endpoint } from '../configs';
 
-export default withApollo(({ initialState, headers = {} }) => {
-  const httpLink = createUploadLink({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : process.env.ENDPOINT,
-    fetchOptions: { credentials: 'include' }
-  });
+export default withApollo(
+  ({ initialState, headers = {} }) => {
+    const httpLink = createUploadLink({
+      uri: process.env.NODE_ENV === 'development' ? endpoint : process.env.ENDPOINT,
+      fetchOptions: { credentials: 'include' }
+    });
 
-  const contextLink = setContext(async () => ({
-    headers
-  }));
+    const contextLink = setContext(async () => ({
+      headers
+    }));
 
-  const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors) {
-      graphQLErrors.map(err => console.log(`[GraphQL error]: Message: ${err.message}`));
-    }
-    if (networkError) console.log(`[Network error]: ${networkError}`);
-  });
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(err => console.log(`[GraphQL error]: Message: ${err.message}`));
+      }
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    });
 
-  const link = ApolloLink.from([errorLink, contextLink, httpLink]);
+    const link = ApolloLink.from([errorLink, contextLink, httpLink]);
 
-  const cache = new InMemoryCache().restore(initialState || {});
+    const cache = new InMemoryCache().restore(initialState || {});
 
-  return new ApolloClient({ link, cache });
-});
+    return new ApolloClient({ link, cache });
+  },
+  { getDataFromTree: 'ssr' }
+);
